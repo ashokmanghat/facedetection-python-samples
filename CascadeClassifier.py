fc8dbe871e5da95545d2612a2fa109de5317d538
Ashok Manghat <ashok.manghat@ii2mail.com>
2019-01-29 17:00:49 +0530

    Added provision to show labels on the detected area

--- CascadeClassifier.py
+++ CascadeClassifier.py
@@ -14,15 +14,18 @@ while(True):
 
     #gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    font = cv2.FONT_HERSHEY_SIMPLEX
     for (x,y,w,h) in faces:
        frame = cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
        roi_gray = gray[y:y+h, x:x+w]
        roi_color = frame[y:y+h, x:x+w]
        eyes = eye_cascade.detectMultiScale(roi_gray)
+       cv2.putText(frame,'Face',(x-2,y), font, 0.4, (0,255,0), 1, cv2.LINE_AA)
        for (ex,ey,ew,eh) in eyes:
            cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
-
-
+           
+       
+   
     # Display the resulting frame
     cv2.imshow('frame',frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
